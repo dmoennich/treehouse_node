c0bd44ea0b028f54a774cbc2d11510e52dc9ed3a
Daniel Moennich <daniel.moennich@gmail.com>
2015-07-19 10:54:10 +0200

    Finished retrieving and parsing badge count and JS points.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var http = require("http");
 
+var userName = "dmoennich";
+
+var printMessage = function(userName, badgeCount, jsPoints){
+	console.log("The user " + userName + " has " + badgeCount + " badges and " + jsPoints + " JavaScript points.");
+};
+
+
 var req = http.get("http://teamtreehouse.com/dmoennich.json", function(res){
-	console.log(res.statusCode);
+	var resBody = "";
+
+	res.on("data", function(dataChunk){
+		resBody += dataChunk;
+	});
+
+	res.on("end", function(){
+		var resJson = JSON.parse(resBody);
+		printMessage(userName, resJson.badges.length, resJson.points.JavaScript);
+	});
 });
 
 
 req.on("error", function(error){
 	console.error("An error occured retrieving the user data: " + error.message);
 });
+
+
