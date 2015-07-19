a488613b3c34c2c036f17ac6dc7d5900d646292f
Daniel Moennich <daniel.moennich@gmail.com>
2015-07-19 11:49:39 +0200

    Added command line arguments support for user names.

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
-var http = require("http");
+var profile = require("./profile");
 
-var userName = "dmoennich";
-
-var printMessage = function(userName, badgeCount, jsPoints){
-	console.log("The user " + userName + " has " + badgeCount + " badges and " + jsPoints + " JavaScript points.");
-};
-
-
-var printErrorMessage = function(error){
-	console.error("An error occured: " + error.message);
-};
-
-
-var req = http.get("http://teamtreehouse.com/" + userName + ".json", function(res){
-	var resBody = "",
-	statusCodeError;
-
-	if(res.statusCode === 200){
-		res.on("data", function(dataChunk){
-			resBody += dataChunk;
-		});
-
-		res.on("end", function(){
-			try{
-				var resJson = JSON.parse(resBody);
-				printMessage(userName, resJson.badges.length, resJson.points.JavaScript);
-			}catch(jsonError){
-				printErrorMessage(jsonError);
-			}
-		});
-	}else {
-		statusCodeError = {
-			message: "Status code " + res.statusCode +
-				" (" + http.STATUS_CODES[res.statusCode] + ") returned for user " + userName + "."
-		};
-		printErrorMessage(statusCodeError);
-	}
-});
-
-
-req.on("error", printErrorMessage);
+var users = process.argv.slice(2);
+users.forEach(profile.get);
 
 
---
+++ profile.js
@@ -0,0 +1,45 @@
+var http = require("http");
+
+var printMessage = function(userName, badgeCount, jsPoints){
+	console.log("The user " + userName + " has " + badgeCount + " badges and " + jsPoints + " JavaScript points.");
+};
+
+
+var printErrorMessage = function(error){
+	console.error("An error occured: " + error.message);
+};
+
+
+var get = function(userName){
+	var req = http.get("http://teamtreehouse.com/" + userName + ".json", function(res){
+		var resBody = "",
+		statusCodeError;
+
+		if(res.statusCode === 200){
+			res.on("data", function(dataChunk){
+				resBody += dataChunk;
+			});
+
+			res.on("end", function(){
+				try{
+					var resJson = JSON.parse(resBody);
+					printMessage(userName, resJson.badges.length, resJson.points.JavaScript);
+				}catch(jsonError){
+					printErrorMessage(jsonError);
+				}
+			});
+		}else {
+			statusCodeError = {
+				message: "Status code " + res.statusCode +
+					" (" + http.STATUS_CODES[res.statusCode] + ") returned for user " + userName + "."
+			};
+			printErrorMessage(statusCodeError);
+		}
+	});
+	req.on("error", printErrorMessage);
+};
+
+
+module.exports.get = get;
+
+
